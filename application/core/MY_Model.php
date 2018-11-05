5cdb629b3a536787c3ba0f9144606864a16aa7fb
Evan Sharp <ego@evansharp.ca>
2018-11-05 14:32:02 -0800

    Add buy/sell constants for sxdata DB

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -4,19 +4,23 @@ class MY_Model extends CI_Model {
 
 	protected $stocks_table = "stocks";
  	protected $segments_table = "segments";
-  protected $ticker_table ="sxdata";
-  protected $users_table = "users";
-  protected $history_table = "trade_history";
-  protected $settings_table = "settings";
-  protected $portfolio_history_table = "portfolio_value_history";
+  	protected $ticker_table ="sxdata";
+  	protected $users_table = "users";
+  	protected $history_table = "trade_history";
+  	protected $settings_table = "settings";
+  	protected $portfolio_history_table = "portfolio_value_history";
 
-  protected $starting_balance = 10000;
+  	protected $starting_balance = 10000;
 
 
 	public function __construct() {
 		parent::__construct();
 
-		define('DB_BUYING', '1');
-		define('DB_SELLING', '0');
+		if( !defined( 'DB_BUYING' ) ){
+			define('DB_BUYING', '1');
+		}
+		if( !defined( 'DB_SELLING' ) ){
+			define('DB_SELLING', '0');
+		}
 	}
 }
