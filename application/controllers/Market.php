a1f1da3d6d4ea9842453aa8b0415dc8a4032b9a9
Evan Sharp <ego@evansharp.ca>
2018-12-10 16:05:25 -0800

    gametick calculations v1
    Segment volitility coefficient is a probability of increase between 'very low' and 50/50, increase and decrease is capped at 11%.

--- application/controllers/Market.php
+++ application/controllers/Market.php
@@ -22,26 +22,47 @@ class Market extends MY_Controller {
 		if( $_GET['key'] == '34d1f91fb2e514b8576fab1a75a89a6b' ){
 
 			$stocks = $this->sm->get_stocks();
-			$new_prices = [];
-			$entropy = act_of_god();
+			$prices = $this->sm->get_all_current_prices();
+			$updates = [];
+
 
 			foreach($stocks as $stock){
-				$price = $stock['']
+				foreach($prices as $price){
+					if($price['stock_id'] == $stock['stock_id']){
+							$current_price = $price['price'];
+					}
+				}
 				$volco = $stock['segment_volitility'];
 				$percent_free = $stock['num_shares'] / $stock['total_shares'];
+
+				//calculate new price
+				$new_price = act_of_god( $volco, $old_price );
+
+				//build array of new prices for stock model update
+				$updates[] = ['stock_id' => $stock_id, 'price' => $new_price];
 			}
+
+			//write new prices to database
+			$this->sm->update_stocks($new_prices);
 
 		}else{
 			return false;
 		}
 	}
 
-	private function act_of_god(){
-		//get random 1-100 number 60% biased greater than 50
-		if (rand(0, 10) > 6){
-    		return rand(50, 100);
+	private function act_of_god( $volco, $old_price){
+		//volco is someshere between 0.01 and 1
+		// 0.1 is barely any chance to change and
+
+
+		if ( ( rand(1, 100) ) /50 > $volco){
+
+			return $old_price * (1 + ( rand(1, 100) / 1000 ) * $volco + ($volco/100) );
+
 		}else{
-    		return rand(1, 49);
+
+    		return $old_price * (1 + ( rand(1, 100) / 1000 ) * $volco );
+
 		}
 	}
 
